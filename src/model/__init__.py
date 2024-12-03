85c96da549607bdc28ee6c3d5cf1018aa88e679b
caaandies <92014368+caaandies@users.noreply.github.com>
2024-12-03 18:00:55 +0300

    init changes

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from src.model.baseline_model import BaselineModel
+from src.model.discriminators.mpd import MPD
+from src.model.discriminators.msd import MSD
+from src.model.generator.generator import Generator
 
-__all__ = [
-    "BaselineModel",
-]
+__all__ = ["BaselineModel", "MPD", "MSD", "Generator"]
