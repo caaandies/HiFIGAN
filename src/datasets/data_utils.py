88624ffa7884c5c83674ce51d9d3a095b17b5333
caaandies <92014368+caaandies@users.noreply.github.com>
2024-12-07 18:58:42 +0300

    get dataloaders logic

--- src/datasets/data_utils.py
+++ src/datasets/data_utils.py
@@ -1,6 +1,7 @@
 from itertools import repeat
 
 from hydra.utils import instantiate
+from torch.utils.data import random_split
 
 from src.datasets.collate import collate_fn
 from src.utils.init_utils import set_worker_seed
@@ -18,55 +19,18 @@ def inf_loop(dataloader):
         yield from loader
 
 
-def move_batch_transforms_to_device(batch_transforms, device):
-    """
-    Move batch_transforms to device.
-
-    Notice that batch transforms are applied on the batch
-    that may be on GPU. Therefore, it is required to put
-    batch transforms on the device. We do it here.
-
-    Batch transforms are required to be an instance of nn.Module.
-    If several transforms are applied sequentially, use nn.Sequential
-    in the config (not torchvision.Compose).
-
-    Args:
-        batch_transforms (dict[Callable] | None): transforms that
-            should be applied on the whole batch. Depend on the
-            tensor name.
-        device (str): device to use for batch transforms.
-    """
-    for transform_type in batch_transforms.keys():
-        transforms = batch_transforms.get(transform_type)
-        if transforms is not None:
-            for transform_name in transforms.keys():
-                transforms[transform_name] = transforms[transform_name].to(device)
-
-
-def get_dataloaders(config, device):
-    """
-    Create dataloaders for each of the dataset partitions.
-    Also creates instance and batch transforms.
-
-    Args:
-        config (DictConfig): hydra experiment config.
-        device (str): device to use for batch transforms.
-    Returns:
-        dataloaders (dict[DataLoader]): dict containing dataloader for a
-            partition defined by key.
-        batch_transforms (dict[Callable] | None): transforms that
-            should be applied on the whole batch. Depend on the
-            tensor name.
-    """
-    # transforms or augmentations init
-    batch_transforms = instantiate(config.transforms.batch_transforms)
-    move_batch_transforms_to_device(batch_transforms, device)
-
+def get_dataloaders(config, device, train_size):
     # dataset partitions init
-    datasets = instantiate(config.datasets)  # instance transforms are defined inside
+    dataset = instantiate(config.datasets)  # instance transforms are defined inside
 
+    train_size = int(train_size * len(dataset))
+    val_size = len(dataset) - train_size
+
+    datasets = {}
+    datasets["train"], dataset["val"] = random_split(dataset, [train_size, val_size])
     # dataloaders init
     dataloaders = {}
+
     for dataset_partition in config.datasets.keys():
         dataset = datasets[dataset_partition]
 
@@ -85,4 +49,4 @@ def get_dataloaders(config, device):
         )
         dataloaders[dataset_partition] = partition_dataloader
 
-    return dataloaders, batch_transforms
+    return dataloaders
