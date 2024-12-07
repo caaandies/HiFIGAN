9b90a85acdac9a6a99d3bc1fe26f4d6947ab946a
caaandies <92014368+caaandies@users.noreply.github.com>
2024-12-07 20:01:59 +0300

    inits changes

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,1 @@
-from src.model.baseline_model import BaselineModel
-from src.model.discriminators.mpd import MPD
-from src.model.discriminators.msd import MSD
-from src.model.generator.generator import Generator
-
-__all__ = ["BaselineModel", "MPD", "MSD", "Generator"]
+from src.model.hifigan import HiFIGAN
--- src/model/discriminators/__init__.py
+++ src/model/discriminators/__init__.py
@@ -1,3 +1,2 @@
 from src.model.discriminators.mpd import MPD
 from src.model.discriminators.msd import MSD
-from src.model.generator.generator import Generator
