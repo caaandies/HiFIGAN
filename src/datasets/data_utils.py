a9902e765701c9ff406ccabd62aee4d468c5095f
caaandies <92014368+caaandies@users.noreply.github.com>
2024-12-08 04:31:26 +0300

    typo fixed

--- src/datasets/data_utils.py
+++ src/datasets/data_utils.py
@@ -27,7 +27,7 @@ def get_dataloaders(config, device, train_size=0.85):
     val_size = len(dataset) - train_size
 
     datasets = {}
-    datasets["train"], dataset["val"] = random_split(dataset, [train_size, val_size])
+    datasets["train"], datasets["val"] = random_split(dataset, [train_size, val_size])
     # dataloaders init
     dataloaders = {}
 
